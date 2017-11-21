350239c8a1d4fa49ae2deefce3193c76a5f29a74
Chema <jose.rodriguez@scmspain.com>
2017-11-21 11:39:50 +0100

    use navlink

--- src/app.js
+++ src/app.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
 
-class App extends React.Component {
+const Home = () => <h1>Home</h1>;
+const About = () => <h1>About</h1>;
+const Contact = () => <h1>Contact</h1>;
 
-  constructor (props) {
-    super(props)
-  }
+const isActivefunc = (match, location) => {
+  console.log(match, location);
+  return false;
+};
 
-  render () {
-    const Home = () => <h1>home</h1>;
-    return (
-        <Router>
-          <Route path="/" component={Home} />
-        </Router>
-    );
-  }
-}
+const Links = () => (
+  <nav>
+    <NavLink exact to="/">Home</NavLink>
+    <NavLink
+      activeStyle={{color: 'green'}}
+      to="/about">About</NavLink>
+    <NavLink
+      isActive={isActivefunc}
+      to="/contact">Contact</NavLink>
+  </nav>
+);
+
+const App = () => (
+  <Router>
+    <div>
+      <Links />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+    </div>
+  </Router>
+);
 
 export default App;
